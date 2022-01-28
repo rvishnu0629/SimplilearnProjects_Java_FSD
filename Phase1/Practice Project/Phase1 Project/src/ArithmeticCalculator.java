f010b97c572c51eb967fbe5fb2bb767188b76888
Vishnu devi R <62171060+rvishnu0629@users.noreply.github.com>
2022-01-28 17:02:21 +0530

    Practice Project

---
+++ Phase1/Practice Project/Phase1 Project/src/ArithmeticCalculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ArithmeticCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2,result ;
+		char operator;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first value");
+		num1= sc.nextInt();
+		System.out.println("Enter the second value");
+		num2=sc.nextInt();
+		System.out.println("Enter the opertaor : + ,- ,*,/,%");
+		operator = sc.next().charAt(0);
+		
+		switch(operator){
+		case '+' :
+			result = num1+num2;
+			System.out.println("Addition:"+ result );
+			break;
+		case '-' :
+			result = num1-num2;
+			System.out.println("Subraction: "+ result);
+			break;
+		case '*':
+			result = num1*num2;
+			System.out.println("Multiplication: "+ result);
+			break;
+		case '/' :
+			result = num1/num2;
+			System.out.println("Division: "+ result);
+			break;
+		case '%' :
+			result = num1%num2;
+			System.out.println("Modulo:" + result);
+			break;
+		default:
+			System.out.println("Invalid Operator");
+			
+		}
+			
+
+	}
+
+}
